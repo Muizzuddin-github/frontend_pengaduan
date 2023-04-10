33a6f585a78a4b71030abebf390e0aabaf0ba676
Muizzuddin-github <muizzuddin334@gmail.com>
2023-04-10 07:59:50 +0700

    menambahkan status

--- src/Components/Pelayanan/Cards.jsx
+++ src/Components/Pelayanan/Cards.jsx
@@ -18,7 +18,7 @@ const Cards = (props) => {
                             <div className="flex mt-14 btn-cards">
                                 <div className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                     {
-                                        props.status == "diterima" ? <ButtonCards btn1='Terima'/> : <ButtonCards btn1='Tangani'/>
+                                        props.status == "terkirim" ? <ButtonCards btn1='Terima'/> : <ButtonCards btn1='Tangani'/>
                                     }
                                 </div>
                                 <div className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
