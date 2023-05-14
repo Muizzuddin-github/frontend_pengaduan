05a8d07b195fb7e7b06fe7939e694542659202ca
GNX56S <ilhanjuliansyah3@gmail.com>
2023-05-14 18:54:25 +0700

    menambahkan form edit dan pop up hapus di List Kategori

--- src/Components/ListKategori/Cards.jsx
+++ src/Components/ListKategori/Cards.jsx
@@ -1,19 +1,35 @@
-const CardsKategori = () => {
+import DeleteConfirm from "./DeleteConfirm"
+import FormUpload from "./Form"
+const CardsKategori = (props) => {
+    const showUbahForm = () => {
+        const tampil = document.querySelector('.tambah-kategori')
+        tampil.classList.remove('hidden')
+        tampil.classList.add('flex')
+      }
+
+    const showConfirm = () => {
+        const tampil = document.querySelector('.hapus-kategori')
+        tampil.classList.remove('hidden')
+    }
     return (
         <>
-        <div className="card card-compact w-72 bg-base-100 shadow-xl bg-black rounded-lg">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="card card-compact w-72 bg-base-100 shadow-xl bg-black rounded-lg m-4">
+            <figure><img src={props.urlFoto} alt="Shoes" /></figure>
             <div className="p-3">
-                <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Judul</h3>
-                <p className="mb-3 font-normal text-black dark:text-white">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.judul}</h3>
+                <p className="mb-3 font-normal text-black dark:text-white">{props.deskripsi}</p>
                 <div className="flex justify-between w-32">
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ubah</a>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Hapus</a>
+                    <button href="#" onClick={showUbahForm} className="ubah-kategori inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ubah</button>
+                    <button href="#" onClick={showConfirm} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Hapus</button>
                 </div>
             </div>
 
         </div>
-
+        <div>
+            <FormUpload judul="Edit Kategori"/>
+            <DeleteConfirm/>
+            
+        </div>
         </>
     );
 }
--- src/ListKategori.jsx
+++ src/ListKategori.jsx
@@ -1,12 +1,34 @@
 import CardsKategori from "./Components/ListKategori/Cards";
-import FormUpload from "./Components/ListKategori/FormAdd";
+import FormUpload from "./Components/ListKategori/Form";
 import Navigasi from "./Components/Menu/Navigasi";
+import { useState } from "react";
 
 const ListKategori = () => {
   const showForm = () => {
     const tampil = document.querySelector('.tambah-kategori')
     tampil.classList.remove('hidden')
+    tampil.classList.add('flex')
   }
+  
+  const [kategori,setKategori] = useState([
+    {
+      judul : "Banjir",
+      deskripsi : "Laporkan Banjir",
+      urlFoto : "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+      judul : "Pohon Tumbang",
+      deskripsi : "Laporkan Pohon Tumbang",
+      urlFoto : "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    },
+    {
+      judul : "Lampu Jalan Mati",
+      deskripsi : "Laporkan Lampu Jalan Mati",
+      urlFoto : "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+    }
+  ])
+
+
   return (
     <>
     <div>
@@ -14,15 +36,21 @@ const ListKategori = () => {
     </div>
     <div className="px-8 py-8"> 
       <div className="flex justify-between items-center mt-5 p-5">
-        <h1 className="font-medium text-2xl">List Kategori</h1>
+        <h1 className="font-bold text-2xl">List Kategori</h1>
         <button onClick={showForm}><i className="fa-solid fa-plus text-3xl text-gray-600"></i></button>
       </div>
       <div className="px-4 py-4 flex flex-wrap ">
-        <CardsKategori/>
-        <CardsKategori/>
+        {
+          kategori.map((x,i)=>(
+            <CardsKategori key={i}
+            judul = {x.judul}
+            deskripsi = {x.deskripsi}
+            urlFoto = {x.urlFoto}/>
+          ))
+        }
       </div>
-      <div>
-        <FormUpload/>
+      <div className="tambah">
+        <FormUpload judul="Tambah Kategori Baru"/>
       </div>
     </div>
     </>
