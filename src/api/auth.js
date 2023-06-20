e8b1c0ed0d4d1fab50f516f9cd47ec99063bb2de
muiz332 <muizzuddin332@gmail.com>
2023-06-20 14:38:13 +0700

    mengubah endpoint logout menjadi post

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,7 @@ import axiosIns from "./axiosInstance";
 const auth = {
   login: (data) => axiosIns.post("/users/login", data),
   daftar: (data) => axiosIns.post("/users/register", data),
-  logout: () => axiosIns.put("/users/logout"),
+  logout: () => axiosIns.post("/users/logout"),
   isLogin: () => axiosIns.get("/users/islogin"),
 };
 
